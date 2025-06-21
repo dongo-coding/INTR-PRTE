4435ffe242ff59c7bab71e8d5d8624516a6d304c
Do Ngo <bahuynh619@gmail.com>
2025-06-22 01:23:12 +0700

    Update arr-1D.cpp

--- arr-1D.cpp
+++ arr-1D.cpp
@@ -115,5 +115,30 @@ int main() {
 }
 
 
+#include <iostream>
+
+using namespace std;
+
+void show(string a[1000], int n) {
+	for(int i=0;i<n;i++){
+        if(a[i].size()>=3){
+            cout<<a[i]<<" ";
+        }
+    }
+}
+
+int main() {
+	int n;
+	string a[1000];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	show(a, n);
+	return 0;
+}
+
+
+
 
 
