ee81868cea78954c9db1f7897fa8c80ca3ca7d7e
Do Ngo <bahuynh619@gmail.com>
2025-06-21 23:07:31 +0700

    Update Min&Max.cpp

--- Min&Max.cpp
+++ Min&Max.cpp
@@ -9,3 +9,15 @@ int main()
     if (b > c) swap(b, c);
     cout << a << " " << c;
 }
+
+**Advanced
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a,b,c;
+    cin>>a>>b>>c;
+    int max = (a>b && a>c) ? a: (b>c ? b : c); //find max by "toan tu 3 ngoi"
+    cout<<max;
+}
