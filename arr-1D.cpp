f6474fcbfa9cb42b4b89d509e66224b543804f41
Do Ngo <bahuynh619@gmail.com>
2025-06-22 01:16:57 +0700

    Update arr-1D.cpp

--- arr-1D.cpp
+++ arr-1D.cpp
@@ -82,3 +82,38 @@ int main() {
 
 
 
+
+#include <iostream>
+
+using namespace std;
+
+int maxOfArray(int a[1000], int n) {
+    int maxVal=a[0];
+	for(int i=0;i<n;i++){
+        if(a[i]>maxVal){
+            maxVal=a[i];
+        }
+    }
+    return maxVal;
+}
+
+int main() {
+	int n1, n2;
+	int a1[1000], a2[1000];
+	cin >> n1;
+	for (int i = 0; i < n1; i++) {
+		cin >> a1[i];
+	}
+	cin >> n2;
+	for (int i = 0; i < n2; i++) {
+		cin >> a2[i];
+	}
+
+	cout << maxOfArray(a1, n1) << " " << maxOfArray(a2, n2);
+
+	return 0;
+}
+
+
+
+
