90d375ff2421191a2cbe975303620f1b32df88c1
Do Ngo <bahuynh619@gmail.com>
2025-06-22 00:31:34 +0700

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -9,9 +9,30 @@ int main() {
     int cnt=0;
     for(int i=0;i<s.size();i++){
         if(s[i]==c){
-            cnt++;      //counting char c such that appeared in string s;
+            cnt++;      //counting char c such that appeared in string s
         }
     }
     cout<<cnt;
+    return 0;
+}
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    char c;
+    cin >> s >> c;
+    int index=-1;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==c){
+            index=i;  //find first pos such that char c appear in string s
+            break;
+        }
+    }
+    cout<<index;
     return 0;
 }
