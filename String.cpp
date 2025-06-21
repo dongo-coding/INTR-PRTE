c1058d2fe46f78728b3b52d5e6fc04ba20979df2
Do Ngo <bahuynh619@gmail.com>
2025-06-22 00:36:23 +0700

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -36,3 +36,21 @@ int main() {
     cout<<index;
     return 0;
 }
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='a' && s[i]<='z'){   
+            s[i]-=32;                    //printed string to regular string
+        }
+    }
+    cout<<s;
+    return 0;
+}
