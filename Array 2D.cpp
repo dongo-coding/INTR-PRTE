6b7b12f6dbeec03758ed6195f24478f6dcb84915
Do Ngo <bahuynh619@gmail.com>
2025-06-22 01:12:41 +0700

    Update Array 2D.cpp

--- Array 2D.cpp
+++ Array 2D.cpp
@@ -54,35 +54,3 @@ int main() {
 
 
 
-#include <iostream>
-
-using namespace std;
-
-void sortArray(int a[1000], int n) {
-	for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i] < a[j]){
-                int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
-}
-
-int main() {
-	int n;
-	int a[1000];
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-	sortArray(a, n);
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << " ";
-	}
-
-	return 0;
-}
-
-
